396e1d91a9b868c3ea0e0091c9eda7a3d1ecafda
Victor Lopes <contato_victorlopes@hotmail.com>
2018-04-27 14:25:45 -0400

    complementando o layout de login

---
+++ app/src/main/java/com/example/mrlopito/grupella/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.mrlopito.grupella;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.nio.file.Files;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText edtEmailUser;
+    private EditText edtPassUser;
+    private Button btnPassUser;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edtEmailUser = findViewById(R.id.edtEmail);
+        edtPassUser = findViewById(R.id.edtSenha);
+        btnPassUser = findViewById(R.id.btnLogin);
+    }
+
+    /*A login provosory method*/
+    public void loginProvisorio(View v){
+        if(edtEmailUser.getText().equals("dev@dev.com") && edtPassUser.getText().equals("admin"))
+            startActivity(new Intent(this, MainActivity.class));
+        else
+            toastAlert("Usuário não autorizado!");
+    }
+    public void toastAlert(String textToAlert){
+        if(textToAlert!=null || textToAlert.equals(""))
+            Toast.makeText(LoginActivity.this, textToAlert, Toast.LENGTH_SHORT);
+    }
+}
