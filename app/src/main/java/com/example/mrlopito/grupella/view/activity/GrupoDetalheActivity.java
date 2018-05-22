fe7dd1aeba00a913225c1fa2944ae13b912c54ba
Gian Fonseca <gianextensao@gmail.com>
2018-05-22 15:06:29 -0400

    [código] correção da listagem

--- app/src/main/java/com/example/mrlopito/grupella/view/activity/GrupoDetalheActivity.java
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/GrupoDetalheActivity.java
@@ -1,12 +1,21 @@
 package com.example.mrlopito.grupella.view.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mrlopito.grupella.R;
+import com.example.mrlopito.grupella.model.entity.Grupo;
+import com.example.mrlopito.grupella.model.service.PicassoClient;
 import com.example.mrlopito.grupella.view.adapter.FragmentAdapter;
 import com.example.mrlopito.grupella.view.fragment.FragmentNavigator;
+import com.example.mrlopito.grupella.view.fragment.MainFragment;
 import com.example.mrlopito.grupella.view.widget.BottomNavigatorView;
 
 public class GrupoDetalheActivity extends AppCompatActivity implements BottomNavigatorView.OnBottomNavigatorViewItemClickListener {
@@ -16,6 +25,13 @@ public class GrupoDetalheActivity extends AppCompatActivity implements BottomNav
     private FragmentNavigator mNavigator;
 
     private BottomNavigatorView bottomNavigatorView;
+
+    private TextView nome;
+    private TextView descricao;
+    private ImageView imagem;
+
+    private Grupo grupo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +49,41 @@ public class GrupoDetalheActivity extends AppCompatActivity implements BottomNav
         }
 
         setCurrentTab(mNavigator.getCurrentPosition());
+
+        initView();
+    }
+
+    public void initView(){
+
+        nome = (TextView)
+                findViewById(R.id.detalhe_grupo_nome);
+
+        imagem = (ImageView)
+                findViewById(R.id.detalhe_grupo_imagem);
+
+
+        this.grupo =
+                (Grupo) getIntent().getSerializableExtra("grupo");
+
+
+        Context c = this.getBaseContext();
+        nome.setText(this.grupo.getNome());
+        PicassoClient.downloadImage(c, this.grupo.getPhotoURL(), imagem);
+
+
+
+        Bundle bundle = new Bundle();
+        bundle.putString("grupo", "test");
+        new MainFragment().setArguments(bundle);
+
+    }
+
+    public void onFragmentViewCreated(View view) {
+        // Iniciar os campos buscando no layout do Fragment
+        descricao
+                = (TextView) view.findViewById(R.id.detalhe_grupo_descricao);
+
+        descricao.setText(this.grupo.getDescricao());
     }
 
     @Override
