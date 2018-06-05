60417e80167cb8b3a230ef6fc8a41c1ae870ab8b
Victor Lopes <contato_victorlopes@hotmail.com>
2018-06-05 19:31:02 -0400

    Adicionando view login

---
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/ActivityCadastro.java
@@ -0,0 +1,15 @@
+package com.example.mrlopito.grupella.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mrlopito.grupella.R;
+
+public class ActivityCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/com/example/mrlopito/grupella/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/LoginActivity.java
@@ -4,38 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.mrlopito.grupella.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText edtEmailUser;
-    private EditText edtPassUser;
-    private Button btnPassUser;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edtEmailUser = findViewById(R.id.edtEmail);
-        edtPassUser = findViewById(R.id.edtSenha);
-        btnPassUser = findViewById(R.id.btnLogin);
     }
 
-    /*A login provosory method*/
-    public void loginProvisorio(View v){
-        if(edtEmailUser.getText().equals("dev@dev.com") && edtPassUser.getText().equals("admin"))
-            startActivity(new Intent(this, MainActivity.class));
-        else
-            toastAlert("Usuário não autorizado!");
+    public void AbrirHome(View view) {
+        Intent it = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(it);
     }
-    public void toastAlert(String textToAlert){
-        if(textToAlert!=null || textToAlert.equals(""))
-            Toast.makeText(LoginActivity.this, textToAlert, Toast.LENGTH_SHORT);
+    public void AbrirCadastro(View view){
+        Intent it = new Intent(LoginActivity.this, ActivityCadastro.class);
+        startActivity(it);
     }
+
 }
