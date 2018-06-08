30cb462e298979438454a790361f43ca5e5b8da1
Victor Lucas Lopes Silva <201519060491@DOMINIO.FACOM.UFMS.BR>
2018-06-08 17:48:51 -0400

    Iniciando cadastro de grupos

--- app/src/main/java/com/example/mrlopito/grupella/view/activity/HomeActivity.java
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.mrlopito.grupella.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.mrlopito.grupella.NewGroup;
 import com.example.mrlopito.grupella.R;
 
 public class HomeActivity extends AppCompatActivity
@@ -28,9 +30,9 @@ public class HomeActivity extends AppCompatActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent it = new Intent(HomeActivity.this, NewGroup.class);
+                startActivity(it);
             }
         });
 
