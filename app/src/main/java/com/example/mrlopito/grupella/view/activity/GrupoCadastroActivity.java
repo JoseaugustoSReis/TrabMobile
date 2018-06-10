08f4b7fa0a590c67450e2df935aaa474d5bf07b7
Victor Lopes <contato_victorlopes@hotmail.com>
2018-06-10 12:16:11 -0400

    criação de grupos iniciada

--- app/src/main/java/com/example/mrlopito/grupella/view/activity/GrupoCadastroActivity.java
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/GrupoCadastroActivity.java
@@ -1,7 +1,7 @@
 package com.example.mrlopito.grupella.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,18 +12,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.mrlopito.grupella.R;
-import com.example.mrlopito.grupella.helper.Base64Custom;
-import com.example.mrlopito.grupella.helper.Preferences;
 import com.example.mrlopito.grupella.model.dao.ConfiguracaoFirebase;
 import com.example.mrlopito.grupella.model.entity.Grupo;
-import com.example.mrlopito.grupella.model.entity.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 
 public class GrupoCadastroActivity extends AppCompatActivity {
@@ -66,7 +56,11 @@ public class GrupoCadastroActivity extends AppCompatActivity {
                 editDescricao.getText().toString(),
                 cboxPublico.isChecked());
 
-        insert(grupo);
+        if(insert(grupo)){
+            Intent it = new Intent(GrupoCadastroActivity.this, ChatActivity.class);
+            startActivity(it);
+        }
+
 
     }
 
