07d46087b20b9830bcc485d1dd5f3ce764a28918
JoseaugustoSReis <joseaugusto44@outlook.com>
2018-06-26 17:08:09 -0400

    message

--- app/src/main/java/com/example/mrlopito/grupella/view/activity/ChatActivity.java
+++ app/src/main/java/com/example/mrlopito/grupella/view/activity/ChatActivity.java
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mrlopito.grupella.R;
+import com.example.mrlopito.grupella.model.dao.ConfiguracaoFirebase;
+import com.example.mrlopito.grupella.model.entity.Grupo;
+import com.example.mrlopito.grupella.model.entity.Message;
+import com.google.firebase.database.DatabaseReference;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private DatabaseReference firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
     }
+
+
 }
